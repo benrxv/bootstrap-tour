f7456fad0f3bd22d7dd4edb360b084b464a1f31a
ben <benrxv@gmail.com>
2014-10-29 21:01:30 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name = "bootstrap-tour",
+    packages = ["bootstrap-tour", ],
+    version = "0.8.0",
+    description = "Quick and easy product tours with Twitter Bootstrap Popovers",
+    author = "sorich87",
+    url = "http://bootstraptour.com",
+)
